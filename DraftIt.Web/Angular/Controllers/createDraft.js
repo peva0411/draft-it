4ebcd7752edb6466cd67283d3214185637a2fd70
Philip Evans <pevans@localedge.com>
2014-11-22 09:41:21 -0500

    added create category and played with ngAnimate

---
+++ DraftIt.Web/Angular/Controllers/createDraft.js
@@ -0,0 +1,17 @@
+﻿angular.module("draftIt").controller("createDraft", ["$scope", "$state", "draft", "category", function ($scope, $state, draft, category) {
+
+    $scope.categories = category.categories;
+
+    $scope.create = function () {
+        
+            draft.drafts.push({
+                id: $scope.id,
+                name: $scope.name,
+                category: $scope.category
+            });
+
+        $state.go('drafts');
+
+        };
+    }
+]);
--- DraftIt.Web/Angular/Controllers/drafts.js
+++ DraftIt.Web/Angular/Controllers/drafts.js
@@ -1,20 +1,6 @@
 ﻿angular.module("draftIt")
     .controller("drafts", [
-        "$scope", function($scope) {
-            $scope.drafts = [
-                {
-                    id: 1,
-                    name: "draft1",
-                    category: "food"
-                }, {
-                    id: 2,
-                    name: "draft2",
-                    category: "animal"
-                }, {
-                    id: 3,
-                    name: "draft3",
-                    category: "beer"
-                }
-            ];
-        }
+        "$scope", "draft", function($scope, draft) {
+        $scope.draft = draft;
+    }
 ]);
